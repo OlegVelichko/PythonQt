6227785c0658a2f9d0be31e97f56fe6d190df6f4
Oleg <vielichko1997@mail.ru>
2017-02-13 14:15:40 +0200

    Update iterator.py

--- iterator.py
+++ iterator.py
@@ -1,22 +1,30 @@
-class Iterator(object):
-
-    def __init__(self, low, high):
-        self.low = low
-        self.high = high
+class ReverseIterator(object):
+    def __init__(self, iterable_object):
+        self.list = iterable_object
+        self.index = len(iterable_object)
 
     def __iter__(self):
         return self
 
-    def __next__(self):
-        if self.low > self.high:
+    def next(self):
+        if self.index == 0:
             raise StopIteration
-        else:
-            self.low += 1
-            return self.low - 1
-        
+        self.index = self.index - 1
+        return self.list[self.index]
+
+    __next__ = next
+
+
+class Days(object):
+
+    def __init__(self):
+        self.days = ["Monday","Tuesday","Wednesday","Thursday",
+                     "Friday","Saturday","Sunday"]
+
+    def reverse_iter(self):
+        return ReverseIterator(self.days)
+
 if __name__ == "__main__":
-
-    show = Iterator(1, 7)
-
-    for i in show:
-        print(i, end=' ')
+    days = Days()
+    for day in days.reverse_iter():
+        print(day, end=' ')
